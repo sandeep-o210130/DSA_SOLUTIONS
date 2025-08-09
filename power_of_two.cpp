3916eb3023fe1fc1f497f4495c5bf4cb5fbfbe5c
sandeep-o210130 <psandeepkumark31@gmail.com>
2025-08-09 18:25:46 +0530

    Update power_of_two.cpp

--- power_of_two.cpp
+++ power_of_two.cpp
@@ -1,8 +1,10 @@
-// power of two not work for negative number you dont assume -2^4==(-ve) its wrong we just use power not put -ve so only for postitve ones.
+// power of two not work for negative number you dont assume -2^4==(-ve) 
+// its wrong we just use power not put -ve so only for postitve ones.
 
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
         return n>0 && (n&(n-1))==0;
     }
+
 };
